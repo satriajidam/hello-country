7bd2765c6e3a0b0174a6aaa3c6ec64bc3d2d97bc
Agastyo Satriaji Idam <agastyo@ruangguru.com>
2019-04-21 10:25:10 +0700

    Add chi middleware

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 var country = os.Getenv("COUNTRY")
@@ -13,6 +15,12 @@ var httpPort = os.Getenv("HTTP_PORT")
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(30 * time.Second))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(
